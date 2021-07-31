1fdda6658f8853ddc49a1f0d2caf5ee4c40428fa
ZerolCamb <zerolcamb@outlook.com>
2021-07-30 23:19:22 -0500

    Programas base
    Se añadieron los primeros dos programas testeados en la ESP32, el primero hacia parpadear repetidamente el LED integrado de la placa y el segundo nos daba la visualización de la camara

---
+++ flash_led/flash_led.ino
@@ -0,0 +1,17 @@
+/* Este programa se compilo para hacer parpadear
+ * el LED de la placa ESP-32 CAM
+ */
+ 
+//LED BUILD_IN is GPIO 4
+void setup() {
+  pinMode(4, OUTPUT); // Set the pin as output
+}
+
+// Remember that the pin work with inverted logic
+// LOW to Turn on and HIGH to turn off
+void loop() {
+  digitalWrite(4, LOW); //Turn on
+  delay (1000); //Wait 1 sec
+  digitalWrite(4, HIGH); //Turn off
+  delay (1000); //Wait 1 sec
+}
