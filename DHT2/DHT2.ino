39be0155b560dc5f5897c54cb9779d182ffbf941
ZerolCamb <zerolcamb@outlook.com>
2021-08-10 19:43:28 -0500

    aver

--- DHT2/DHT2.ino
+++ DHT2/DHT2.ino
@@ -1,32 +1,49 @@
-#include "DHT.h"
+/* Conexión con el modulo DHT11 con la ESP32-CAM
+ * Por: Paloma Vilchis
+ * Modificado por: Cesar Arturo Mejia Bazan
+ *
+ * Este programa realiza una conexión con el módulo  DHT11 de temperatura
+ * Componente     PinESP32CAM
+ * DHT11----------GPIO 12
+ */
+#include "DHT.h" /*Se añade la libreria DHT*/
 
+<<<<<<< HEAD
 #define DHTPIN 13   
+=======
+#define DHTPIN 12  /*Se define el pin en donde estara conectado el módulo*/
+>>>>>>> 821a78d84f28ba4c8f3bc8b9d004e6136ba41f21
 
-#define DHTTYPE DHT11   
+#define DHTTYPE DHT11  /*Se define el tipo de módulo DHT, en este caso es el DHT11*/
 
  
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE); /*Se crea un objeto dht de la clase DHT con el pin y el tipo de módulo definidos anteriormente*/
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println(F("Prueba de conexion"));
+  Serial.begin(115200); /*Se comienza declarando un serial.begin para imprimir en la pantalla debug*/
+  Serial.println(F("Prueba de conexion")); /*Se imprime "Prueba de conexion", se utiliza la funcion F() para enviar una cadena a la memoria del programa en lugar de la RAM de la placa*/ 
 
-  dht.begin();
+  dht.begin(); /*Se inicializa el módulo dht*/
 }
 
 void loop() {
   delay(2000);
 
-  float t = dht.readTemperature();
-  float f = dht.readTemperature(true);
+  float t = dht.readTemperature(); /*Se lee la temperatura del dht en °C*/
+  float f = dht.readTemperature(true); /*Se lee la temperatura del dht en °F*/
   
-  if ( isnan(t) || isnan(f)) {
-    Serial.println(F("No hay conexion"));
-    return;
+  if ( isnan(t) || isnan(f)) { /*En caso de que alguno de los dos valores NO sea numerico*/
+    Serial.println(F("No hay conexion")); /*Se imprimira que no hay conexión con el módulo*/
+    return; /*Se termina el programa*/
   }
 
+<<<<<<< HEAD
  
   Serial.println(F("Temperatura en °C"));
+=======
+  /*Se imprimen las temperaturas en °C y °F*/
+  Serial.println(F("Temperatura en °C "));
+>>>>>>> 821a78d84f28ba4c8f3bc8b9d004e6136ba41f21
   Serial.print(t);
   Serial.println(F("Temperatura en °F"));
   Serial.print(f);
